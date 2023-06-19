8689f63755c0c5bd0329eb598f382bb10373053a
Tonii663 <brown.tonii663@gmail.com>
2023-06-19 17:42:09 -0400

    New

---
+++ custom-vars.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4fecab6b55e469bf7cbd3ff300d67309c40b78af3341a0b3a9610b5a5154b07f" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" "bf948e3f55a8cd1f420373410911d0a50be5a04a8886cabe8d8e471ad8fdba8e" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "ddffe74bc4bf2c332c2c3f67f1b8141ee1de8fd6b7be103ade50abb97fe70f0c" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "eca44f32ae038d7a50ce9c00693b8986f4ab625d5f2b4485e20f22c47f2634ae" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "570263442ce6735821600ec74a9b032bc5512ed4539faf61168f2fdf747e0668" "2e05569868dc11a52b08926b4c1a27da77580daa9321773d92822f7a639956ce" "ce4234c32262924c1d2f43e6b61312634938777071f1129c7cde3ebd4a3028da" "4fda8201465755b403a33e385cf0f75eeec31ca8893199266a6aeccb4adedfa4" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "f458b92de1f6cf0bdda6bce23433877e94816c3364b821eb4ea9852112f5d7dc" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "49acd691c89118c0768c4fb9a333af33e3d2dca48e6f79787478757071d64e68" "b99e334a4019a2caa71e1d6445fc346c6f074a05fcbb989800ecbe54474ae1b0" "251ed7ecd97af314cd77b07359a09da12dcd97be35e3ab761d4a92d8d8cf9a71" default))
+ '(package-selected-packages
+   '(straight org-roam-ui org-roam browse-kill-ring ivy-xref pacmacs langtool simple-modeline corfu yasnippet typescript-mode dumb-jump ag highlight-numbers gcmh zen-mode bug-hunter eglot elgot writeroom-mode uniquify resize-window rainbow-delimiters adwaita-dark-theme quelpa-use-package quelpa page-break-lines rust-mode zig-mode ess mood-line dashboard peep-dired spaceline spaceline-config neotree filetree pdf-tools mic-paren skewer-mode emmet-mode web-mode rjsx-mode good-scroll smooth-scrolling glsl-mode ivy-prescient which-key use-package rainbow-mode python-mode org-bullets omnisharp magit lsp-mode ivy-rich helpful eshell-info-banner elcord doom-themes doom-modeline diminish counsel-projectile company ace-jump-mode))
+ '(warning-suppress-log-types
+   '((initialization)
+	 (initialization)
+	 (use-package)
+	 (use-package)))
+ '(warning-suppress-types '((initialization) (use-package) (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -1,4 +1,67 @@
-(org-babel-load-file (expand-file-name "config-new.org" user-emacs-directory))
-(put 'downcase-region 'disabled nil)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+(straight-use-package 'use-package) ; Base package for package configuration
+(use-package straight
+  :custom
+  (straight-use-package-by-default t "Auto install package use in the configuration"))
+
+
+;; Configure package.el to include MELPA.
+;; (require 'package)
+;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;; (package-initialize)
+
+;; Ensure that use-package is installed.
+;;
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+;; (when (not (package-installed-p 'use-package))
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
+;; Quelpa
+;; (use-package quelpa
+;;   :custom
+;;   (quelpa-update-melpa-p nil "Don't update the MELPA git repo"))
+;; (use-package quelpa-use-package)
+
+(setq use-package-always-defer t)
+
+
+(defvar rebuild-config nil "If true rebuilds the config org file otherwise use the exisiting config.el")
+
+(if rebuild-config
+	(progn
+	  (message "Rebuilt")
+	  (org-babel-load-file (expand-file-name "config-new.org" user-emacs-directory)))
+  (progn
+	(message "Load exisiting config")
+	(load-file "~/.emacs.d/config-new.el")))
+
+;; (org-babel-load-file (expand-file-name "config-new.org" user-emacs-directory))
+
+;; (byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
 ;; (load-file "~/.emacs.d/config-new.el")
+
+;; (put 'narrow-to-region 'disabled nil)
+;; (put 'upcase-region 'disabled nil)
